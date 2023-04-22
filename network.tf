5c8ba10ef45b7783ea455bf9e1224fe5e7b8e135
kodaikumatani <koudai.aco5423@gmail.com>
2023-04-23 03:19:01 +0900

    feat: ecsの追加

--- network.tf
+++ network.tf
@@ -28,12 +28,12 @@ resource "aws_vpc" "main" {
 # https://www.terraform.io/docs/providers/aws/r/subnet.html
 resource "aws_subnet" "public_1a" {
   # 先程作成したVPCを参照し、そのVPC内にSubnetを立てる
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   # Subnetを作成するAZ
   availability_zone = "ap-northeast-1a"
 
-  cidr_block        = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "handson-public-1a"
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_1a" {
 }
 
 resource "aws_subnet" "public_1c" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   availability_zone = "ap-northeast-1c"
   cidr_block        = "10.0.2.0/24"
@@ -53,7 +53,7 @@ resource "aws_subnet" "public_1c" {
 
 # Private Subnets
 resource "aws_subnet" "private_1a" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   availability_zone = "ap-northeast-1a"
   cidr_block        = "10.0.10.0/24"
@@ -64,7 +64,7 @@ resource "aws_subnet" "private_1a" {
 }
 
 resource "aws_subnet" "private_1c" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   availability_zone = "ap-northeast-1c"
   cidr_block        = "10.0.20.0/24"
@@ -77,7 +77,7 @@ resource "aws_subnet" "private_1c" {
 # Internet Gateway
 # https://www.terraform.io/docs/providers/aws/r/internet_gateway.html
 resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "handson"
@@ -96,8 +96,8 @@ resource "aws_eip" "nat_1a" {
 
 # NAT Gateway 1a
 resource "aws_nat_gateway" "nat_1a" {
-  subnet_id     = "${aws_subnet.public_1a.id}" # NAT Gatewayを配置するSubnetを指定
-  allocation_id = "${aws_eip.nat_1a.id}"       # 紐付けるElasti IP
+  subnet_id     = aws_subnet.public_1a.id # NAT Gatewayを配置するSubnetを指定
+  allocation_id = aws_eip.nat_1a.id       # 紐付けるElasti IP
 
   tags = {
     Name = "handson-1a"
@@ -115,8 +115,8 @@ resource "aws_eip" "nat_1c" {
 
 # NAT Gateway 1c
 resource "aws_nat_gateway" "nat_1c" {
-  subnet_id     = "${aws_subnet.public_1c.id}"
-  allocation_id = "${aws_eip.nat_1c.id}"
+  subnet_id     = aws_subnet.public_1c.id
+  allocation_id = aws_eip.nat_1c.id
 
   tags = {
     Name = "handson-1c"
@@ -126,7 +126,7 @@ resource "aws_nat_gateway" "nat_1c" {
 # Route Table
 # https://www.terraform.io/docs/providers/aws/r/route_table.html
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "handson-public"
@@ -137,26 +137,26 @@ resource "aws_route_table" "public" {
 # https://www.terraform.io/docs/providers/aws/r/route.html
 resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = "${aws_route_table.public.id}"
-  gateway_id             = "${aws_internet_gateway.main.id}"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
 # Association
 # https://www.terraform.io/docs/providers/aws/r/route_table_association.html
 resource "aws_route_table_association" "public_1a" {
-  subnet_id      = "${aws_subnet.public_1a.id}"
-  route_table_id = "${aws_route_table.public.id}"
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_1c" {
-  subnet_id      = "${aws_subnet.public_1c.id}"
-  route_table_id = "${aws_route_table.public.id}"
+  subnet_id      = aws_subnet.public_1c.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Route Table (Private)
 # https://www.terraform.io/docs/providers/aws/r/route_table.html
 resource "aws_route_table" "private_1a" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "handson-private-1a"
@@ -164,7 +164,7 @@ resource "aws_route_table" "private_1a" {
 }
 
 resource "aws_route_table" "private_1c" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "handson-private-1c"
@@ -176,24 +176,24 @@ resource "aws_route_table" "private_1c" {
 # https://www.terraform.io/docs/providers/aws/r/route.html
 resource "aws_route" "private_1a" {
   destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = "${aws_route_table.private_1a.id}"
-  nat_gateway_id         = "${aws_nat_gateway.nat_1a.id}"
+  route_table_id         = aws_route_table.private_1a.id
+  nat_gateway_id         = aws_nat_gateway.nat_1a.id
 }
 
 resource "aws_route" "private_1c" {
   destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = "${aws_route_table.private_1c.id}"
-  nat_gateway_id         = "${aws_nat_gateway.nat_1c.id}"
+  route_table_id         = aws_route_table.private_1c.id
+  nat_gateway_id         = aws_nat_gateway.nat_1c.id
 }
 
 # Association (Private)
 # https://www.terraform.io/docs/providers/aws/r/route_table_association.html
 resource "aws_route_table_association" "private_1a" {
-  subnet_id      = "${aws_subnet.private_1a.id}"
-  route_table_id = "${aws_route_table.private_1a.id}"
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private_1a.id
 }
 
 resource "aws_route_table_association" "private_1c" {
-  subnet_id      = "${aws_subnet.private_1c.id}"
-  route_table_id = "${aws_route_table.private_1c.id}"
+  subnet_id      = aws_subnet.private_1c.id
+  route_table_id = aws_route_table.private_1c.id
 }
