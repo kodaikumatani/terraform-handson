b643d816caa5da5688f30da9ff8fe6cc698344aa
kodaikumatani <koudai.aco5423@gmail.com>
2023-04-22 21:31:38 +0900

    feat: vpc関連の作成

---
+++ network.tf
@@ -0,0 +1,199 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "aws-saml-login"
+}
+
+# VPC
+# https://www.terraform.io/docs/providers/aws/r/vpc.html
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+# Public Subnets
+# https://www.terraform.io/docs/providers/aws/r/subnet.html
+resource "aws_subnet" "public_1a" {
+  # 先程作成したVPCを参照し、そのVPC内にSubnetを立てる
+  vpc_id = "${aws_vpc.main.id}"
+
+  # Subnetを作成するAZ
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+
+  tags = {
+    Name = "handson-public-1c"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+# Internet Gateway
+# https://www.terraform.io/docs/providers/aws/r/internet_gateway.html
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+# Elasti IP 1a
+# https://www.terraform.io/docs/providers/aws/r/eip.html
+resource "aws_eip" "nat_1a" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1a"
+  }
+}
+
+# NAT Gateway 1a
+resource "aws_nat_gateway" "nat_1a" {
+  subnet_id     = "${aws_subnet.public_1a.id}" # NAT Gatewayを配置するSubnetを指定
+  allocation_id = "${aws_eip.nat_1a.id}"       # 紐付けるElasti IP
+
+  tags = {
+    Name = "handson-1a"
+  }
+}
+
+# Elasti IP 1c
+resource "aws_eip" "nat_1c" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1c"
+  }
+}
+
+# NAT Gateway 1c
+resource "aws_nat_gateway" "nat_1c" {
+  subnet_id     = "${aws_subnet.public_1c.id}"
+  allocation_id = "${aws_eip.nat_1c.id}"
+
+  tags = {
+    Name = "handson-1c"
+  }
+}
+
+# Route Table
+# https://www.terraform.io/docs/providers/aws/r/route_table.html
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-public"
+  }
+}
+
+# Route
+# https://www.terraform.io/docs/providers/aws/r/route.html
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.public.id}"
+  gateway_id             = "${aws_internet_gateway.main.id}"
+}
+
+# Association
+# https://www.terraform.io/docs/providers/aws/r/route_table_association.html
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = "${aws_subnet.public_1a.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id      = "${aws_subnet.public_1c.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+# Route Table (Private)
+# https://www.terraform.io/docs/providers/aws/r/route_table.html
+resource "aws_route_table" "private_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+
+resource "aws_route_table" "private_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+
+# Route (Private)
+# https://www.terraform.io/docs/providers/aws/r/route.html
+resource "aws_route" "private_1a" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1a.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1a.id}"
+}
+
+resource "aws_route" "private_1c" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1c.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1c.id}"
+}
+
+# Association (Private)
+# https://www.terraform.io/docs/providers/aws/r/route_table_association.html
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = "${aws_subnet.private_1a.id}"
+  route_table_id = "${aws_route_table.private_1a.id}"
+}
+
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = "${aws_subnet.private_1c.id}"
+  route_table_id = "${aws_route_table.private_1c.id}"
+}
